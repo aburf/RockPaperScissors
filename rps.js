f8c3f7003889af55f9030b7792c7e0902e7beddb
Andrew <afburford@gmail.com>
2022-05-08 18:42:45 -0700

    Added ability to play round

--- rps.js
+++ rps.js
@@ -40,3 +40,70 @@ function isValidInput(rpsArray,formattedInput){
 
 console.log("Is Valid input = "+
     isValidInput(rpsOptions,formatCaps(playerSelection)))
+
+
+//Play a round of rock paper scissors
+function playRound(computerInput,playerInput){
+    //format playerInput
+    let frmtPlayerSelection = formatCaps(playerInput)
+    
+
+    let loseWinDraw = ""
+
+    //initialize for later
+    //only play if input is valid (rock paper scissors)
+    if (isValidInput(rpsOptions,frmtPlayerSelection)=== true)
+    {
+        switch(computerInput){
+            case "Rock":
+                switch(frmtPlayerSelection){
+                    case "Rock":
+                        loseWinDraw="draw";
+                        break;
+                    case "Paper":
+                        loseWinDraw="win";
+                        break;
+                    case "Scissors":
+                        loseWinDraw="lose";
+                        break;
+                }
+            case "Paper":
+                switch(frmtPlayerSelection){
+                    case "Rock":
+                        loseWinDraw="lose";
+                        break;
+                    case "Paper":
+                        loseWinDraw="draw";
+                        break;
+                    case "Scissors":
+                        loseWinDraw="win";
+                        break;
+                }
+            case "Scissors":
+                switch(frmtPlayerSelection){
+                    case "Rock":
+                        loseWinDraw="win";
+                        break;
+                    case "Paper":
+                        loseWinDraw="lose";
+                        break;
+                    case "Scissors":
+                        loseWinDraw="draw";
+                        break;
+                }
+
+        }
+        ///give text depending on victory condition
+        switch(loseWinDraw){
+            case "win": return `You win! ${frmtPlayerSelection} beats ${computerInput}.`
+            case "lose": return `You lose! ${computerInput} beats ${frmtPlayerSelection}.`
+            case "draw": return "This one's a draw!"                
+        }
+    }
+    
+    else {return "Invalid input, you big goof."}
+}
+
+
+console.log(playRound(computerSelection(rpsOptions)
+,playerSelection))
