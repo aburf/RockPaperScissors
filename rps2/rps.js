d1a88847f5f8762b4b1614acb54404de54506f6f
Andrew <afburford@gmail.com>
2022-06-05 17:50:28 -0700

    add score update functionality

--- rps2/rps.js
+++ rps2/rps.js
@@ -1,27 +1,65 @@
 
 let userScoreVal = 0;
 let compScoreVal = 0;
+let drawScoreVal = 0;
 
 
 //document.getElementByClass("playerScore") = 0;
 const scoreContainer = document.querySelector('#scoreContainer');
 const userScoreDiv = document.createElement('div');
-const hyphenDiv = document.createElement('div');
+const hyphenDiv1 = document.createElement('div');
 const compScoreDiv = document.createElement('div');
+const hyphenDiv2 = document.createElement('div');
+const drawDiv = document.createElement('div');
 
 userScoreDiv.classList.add('userScoreDiv');
 userScoreDiv.textContent = userScoreVal;
 
-hyphenDiv.classList.add('compScoreVal');
-hyphenDiv.textContent = '-';
+hyphenDiv1.classList.add('hyphenDiv1');
+hyphenDiv1.textContent = '-';
 
-compScoreDiv.classList.add('compScoreVal');
+compScoreDiv.classList.add('compScoreDiv');
 compScoreDiv.textContent = compScoreVal;
 
-scoreContainer.appendChild(userScoreDiv);
-scoreContainer.appendChild(hyphenDiv);
-scoreContainer.appendChild(compScoreDiv);
+hyphenDiv2.classList.add('hyphenDiv2');
+hyphenDiv2.textContent = '-';
 
+drawDiv.classList.add('drawDiv');
+drawDiv.textContent = drawScoreVal;
+
+scoreContainer.appendChild(userScoreDiv);
+scoreContainer.appendChild(hyphenDiv1);
+scoreContainer.appendChild(compScoreDiv);
+scoreContainer.appendChild(hyphenDiv2);
+scoreContainer.appendChild(drawDiv);
+
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    console.log(button.id);
+    let roundResult = playRound(button.id)[2];
+    console.log(roundResult);
+    if (roundResult==='win'){userScoreVal++}
+    else if (roundResult==="lose"){compScoreVal++}
+    else {drawScoreVal++}
+    userScoreDiv.textContent = userScoreVal;
+    compScoreDiv.textContent = compScoreVal;
+    drawDiv.textContent = drawScoreVal;
+  });
+});
+
+/*
+const btn = document.getElementById('btn');
+
+// ✅ Change button text on click
+btn.addEventListener('click', function handleClick() {
+  btn.textContent = 'Button clicked';
+});
+*/
 
 //Randomly select from RPS array, defaults to RPS
 function computerSelection(rpsArray=Array("Rock", "Paper", "Scissors"))
@@ -81,16 +119,7 @@ function promptPlayer(){
 /////////////////////////////////////////////////
 
 
-// buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll('button');
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
-  // and for each one we add a 'click' listener
-  button.addEventListener('click', () => {
-    console.log(button.id);
-    return button.id; 
-  });
-});
+
 
 
 /////////////////////////////////////////////////
@@ -101,9 +130,9 @@ buttons.forEach((button) => {
 
 
 //Play a round of rock paper scissors
-function playRound(){
+function playRound(userInput){
     let computerInput = computerSelection();
-    let frmtPlayerSelection = button.id; //promptPlayer();
+    //let frmtPlayerSelection = button.id; //promptPlayer();
     
     let loseWinDraw = "";
 
@@ -114,8 +143,8 @@ function playRound(){
         ,["Paper","Rock"]
         ]
     
-    if(computerInput===frmtPlayerSelection){
-        return loseWinDraw = [frmtPlayerSelection
+    if(computerInput===userInput){
+        return loseWinDraw = [userInput
                             ,computerInput
                             ,"draw"
                             ];
@@ -124,11 +153,11 @@ function playRound(){
         let iLoopCountNoBreaks = 0; //if no win occurs; going over zero means loss
         for(let i=0; i<=2; i++){
             if(
-                frmtPlayerSelection === winOutcomes[i][0]
+                userInput === winOutcomes[i][0]
                 &&
                 computerInput === winOutcomes[i][1]
                 ){
-                    return loseWinDraw = [frmtPlayerSelection
+                    return loseWinDraw = [userInput
                                         ,computerInput
                                         ,"win"
                                         ];
@@ -139,7 +168,7 @@ function playRound(){
 
         //loss if iLoopCountNoBreaks = 3 with no break
         if(iLoopCountNoBreaks = 3){
-            return loseWinDraw = [frmtPlayerSelection
+            return loseWinDraw = [userInput
                                 ,computerInput
                                 ,"lose"
                                 ];
